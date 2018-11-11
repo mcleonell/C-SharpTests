be4a549b021fc7974ca08ff1dcbe215bb12f885a
Kim MCleonell <mcleonell@gmail.com>
2018-11-11 01:25:01 +0100

    Lay-out OK

---
+++ CreateUserAndLoginTest/CreateUserAndLoginTest.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreateUserAndLoginTest.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string _username = txtUsername.Text.ToLower();
+            string _password = txtPassword.Text;
+
+            User user = new User();
+
+            if (user.Login(_username, _password))
+            {
+                lblError.Content = "";
+                MessageBox.Show("You succesfully loged in!");
+            }
+            else
+            {
+                lblError.Content = "Username and/or password wrong.";
+            }
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            CreateNewUserPage createNewUserPage = new CreateNewUserPage();
+            this.Content = createNewUserPage;
+        }
+    }
+}
---
+++ CreateUserAndLoginTest/CreateUserAndLoginTest.Wpf/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace CreateUserAndLoginTest.Wpf
+{
+    class User
+    {
+        string username = "user";
+        string password = "pass";
+
+        public User()
+        { 
+            
+        }
+
+        public bool Login(string _username, string _password)
+        {
+            if(_username == username && _password == password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CreateNewUser()
+        {
+            // TODO - Add new user to database
+        }
+    }
+}
